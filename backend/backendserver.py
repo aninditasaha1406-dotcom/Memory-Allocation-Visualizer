aa70e4706d6116d50a76c3b7c4a3024a07d5f74a
Anindita Saha <aninditasaha1406@gmail.com>
2026-02-21 11:08:00 +0530

    Let render auto detect port

--- backend/backendserver.py
+++ backend/backendserver.py
@@ -209,5 +209,4 @@ def api_compare_all():
 # RUN SERVER
 # -------------------------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0")
